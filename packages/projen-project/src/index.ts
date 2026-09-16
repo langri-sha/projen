c06f69bd76b6fb243a0e20d485c2b3a7ac6c399e
Filip Dupanovic <filip.dupanovic@gmail.com>
2026-09-16 22:55:23 +0200

    Install Jest only when configured
    Closes #193

--- packages/projen-project/src/index.ts
+++ packages/projen-project/src/index.ts
@@ -642,6 +642,7 @@ export class Project extends BaseProject {
       return
     }
 
+    this.#addDefaultDevDeps('jest@30.4.2')
     this.jestConfig = new JestConfig(this, jestConfigOptions)
 
     this.typeScriptConfig?.addFile(this.jestConfig.path)
